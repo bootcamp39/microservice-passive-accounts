a9c74e9777ff7bc543ae4a271a8b896782b6cb85
csanmont <csanmont@emeal.nttdata.com>
2022-12-18 11:02:38 -0500

    Do refactory of methods

--- src/main/java/com/nttdata/microservice/bankpassiveaccounts/facade/impl/PassiveAccountFacadeImpl.java
+++ src/main/java/com/nttdata/microservice/bankpassiveaccounts/facade/impl/PassiveAccountFacadeImpl.java
@@ -1,9 +1,13 @@
 package com.nttdata.microservice.bankpassiveaccounts.facade.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.nttdata.microservice.bankpassiveaccounts.facade.IPassiveAccountFacade;
 
 import reactor.core.publisher.Mono;
 
+
+@Service
 public class PassiveAccountFacadeImpl implements  IPassiveAccountFacade{
 
 	@Override
